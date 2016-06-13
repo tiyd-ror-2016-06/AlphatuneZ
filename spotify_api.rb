233dfcb17aad7f5c645ac16e560dc8e08988c76b
Zach H <zdhadgraft@gtcc.edu>
2016-06-13 16:09:52 -0400

    Added to spotify_api parser...
    - Added get_song_query
    - Added switch for using test vs. live data

--- spotify_api.rb
+++ spotify_api.rb
@@ -1,36 +1,56 @@
+require 'httparty'
+require 'pry'
+require 'json'
 
-    HTTParty.get "https://api.spotify.com/v1/search?q=Fireflies&type=track", headers: { "Accept" => "application/json", "Authorization" => "Bearer Token_goes_here" }
+Spotify_api = "https://api.spotify.com"
+Token =
+
+
+def get_song_query song_title
+  st_encoded = URI.encode song_title
+  HTTParty.get(
+    Spotify_api + "/v1/search?q=#{st_encoded}&type=track",
+                headers: { "Accept" => "application/json", "Authorization" => "Bearer " + Token }
+  )
+end
+
+
+if ENV["TEST"]
+  raw_data = JSON.parse(File.read "spotifytest1")
+else
+  raw_data = get_song_query("Mr. Blue Sky")
+end
 
 list_of_songs = []
 
 # Some song Details
 
-songs.each do |s|
-list_of_songs.push(s)
-songs.first["album"]["name"]
+# songs.each do |s|
+# list_of_songs.push(s)
+# songs.first["album"]["name"]
 
 
 
-  e = JSON.parse(File.read "spotifytest1")
 
 
-raw_songs = []
-rawdata["tracks"]["items"].each do |f|
-raw_songs.push(f)
-end
+
+# raw_songs = []
+# rawdata["tracks"]["items"].each do |f|
+# raw_songs.push(f)
+# end
 
 
-songs.each do |s|
-  song_hash = {}
-  s.keys.each do |key|
-    case key
-    when "album"
-      #song_hash[key] = s[key]["name"]
-    when "artists"
-      song_hash["artist"] = s[key]["name"]
-    when "id"
-      song_hash["id"] = s[key]["id"]
-    when "name"
-      song_hash["title"] = s[key]["name"]
-    end
-  end
+# songs.each do |s|
+#   song_hash = {}
+#   s.keys.each do |key|
+#     case key
+#     when "album"
+#       #song_hash[key] = s[key]["name"]
+#     when "artists"
+#       song_hash["artist"] = s[key]["name"]
+#     when "id"
+#       song_hash["id"] = s[key]["id"]
+#     when "name"
+#       song_hash["title"] = s[key]["name"]
+#     end
+#   end
