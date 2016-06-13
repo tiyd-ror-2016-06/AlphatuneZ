c3f81ccf4f2218cfc18a9a1736593dc1069f1233
James Dabbs <jamesdabbs@gmail.com>
2016-06-13 13:59:36 -0400

    Sketch user authorization feature API

--- tests.rb
+++ tests.rb
@@ -9,3 +9,36 @@ Minitest::Reporters.use! Minitest::Reporters::ProgressReporter.new
 
 require 'rack/test'
 require './app'
+
+class UserTests < Minitest::Test
+  include Rack::Test::Methods
+
+  def app
+    MyApp
+  end
+
+  def setup
+    User.delete_all
+  end
+
+  def login_as user
+    MyApp::LOGGED_IN_USERS.push user
+  end
+
+  def test_can_fake_logged_in_requests
+    user = User.create! email: "art@example.com", password: "password"
+    login_as user
+
+    response = get "/api/me"
+    assert_equal 200, response.status
+
+    body = JSON.parse response.body
+    assert_equal "art@example.com", body["email"]
+  end
+
+  def test_isnt_logged_in_unless_you_say_so
+    response = get "/api/me"
+
+    assert_equal 401, response.status
+  end
+end
