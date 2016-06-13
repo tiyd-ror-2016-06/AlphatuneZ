92934f07ffa28023026999ef0ee93be6f5b9584f
Jorge Vega Ramirez <jorgevp5@gmail.com>
2016-06-13 14:09:57 -0400

    Before master merge

--- app.rb
+++ app.rb
@@ -27,5 +27,20 @@ class MyApp < Sinatra::Base
     end
   end
 
+  post "/:user/:song/:vote" do
+  binding.pry
+    # if params[:user] == user.id && params[:song] == song.id
+    #   if params[:vote] == up
+    #     vote_val = 1
+    #   else
+    #     vote_val = -1
+    #   end
+    #   Vote.create!(user: user.id, song: song.id, vote: vote_val, placed_at: Date.time)
+    # else
+    #   status 404
+    # end
+  end
+
+
   run! if $PROGRAM_NAME == __FILE__
 end
--- tests.rb
+++ tests.rb
@@ -12,12 +12,13 @@ require './app'
 
 
 
-
+focus
 def test_users_can_upvote_songs
   song = Song.where(title: "Bohemian Rhapsody", artist: "Queen", genre: "Classic Rock").first_or_create!
+  binding.pry
   user = User.where(email: "jorgevp5@gmail.com", password: "password").first_or_create!
   post "/#{user.id}/#{song.id}/up"
-
+  binding.pry
   assert_equal 1, song.votes.value
 
 end
