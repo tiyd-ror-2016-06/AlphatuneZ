cd64d7bb5c4ffddb8b493007decb067ccbd1ec00
Jorge Vega Ramirez <jorgevp5@gmail.com>
2016-06-13 13:53:57 -0400

    Initial commit

--- tests.rb
+++ tests.rb
@@ -9,3 +9,21 @@ Minitest::Reporters.use! Minitest::Reporters::ProgressReporter.new
 
 require 'rack/test'
 require './app'
+
+
+
+
+def test_users_can_upvote_songs
+  song = Song.where(title: "Bohemian Rhapsody", artist: "Queen", genre: "Classic Rock").first_or_create!
+  user = User.where(email: "jorgevp5@gmail.com", password: "password").first_or_create!
+  post "/#{user.id}/#{song.id}/up"
+
+  assert_equal 1, song.votes.value
+
+end
+
+
+def test_users_can_downvote_songs
+
+
+end
