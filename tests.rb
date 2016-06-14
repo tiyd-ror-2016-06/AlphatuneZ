1f871c578b40fc37eac02cbc8f35bcbd7f0efc2c
James Dabbs <jamesdabbs@gmail.com>
2016-06-14 11:43:34 -0400

    Merge test files

--- tests.rb
+++ tests.rb
@@ -170,3 +170,18 @@ class UserTests < Minitest::Test
   end
 
 end
+
+
+class TestSongOrder < Minitest::Test
+
+  def setup
+    [User, Song].each { |klass| klass.delete_all }
+  end
+
+  def test_can_standardize_song_names
+    Song.create!(title: "(($%#R))",artist: "Xenu",genre: "Scientology Hymn", suggester_id: 1)
+    song_title = Song.first.standardized_title
+
+    assert_equal "R", song_title
+  end
+end
