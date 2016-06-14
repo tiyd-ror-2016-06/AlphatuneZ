738310265a6d3c996b57528c1793956426d4aeb2
Maria Cassino <maria.cassino@gmail.com>
2016-06-14 16:55:55 -0400

    generates refresh authorization tokens

--- spotify_api_tester.rb
+++ spotify_api_tester.rb
@@ -5,6 +5,12 @@ require './spotify_api.rb'
 
 spotify = SpotifyApiRequest.new(song: "This is a song")
 
-spotify.parse!
-songs = spotify.get_songs
+
+# spotify.parse!
+# songs = spotify.get_songs
+# g = spotify.get_song_query
+
+generate = spotify.generate_beginning_token
+refresh = spotify.refresh_access_token
+t = spotify.token
 binding.pry
