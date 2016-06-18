227e694695f3c6d2159befdf94cfef6a3cbb0aa2
Zach H <zdhadgraft@gtcc.edu>
2016-06-18 19:43:06 -0400

    Cleaned up SpotifyApiToken.

--- spotify_api_token.rb
+++ spotify_api_token.rb
@@ -4,19 +4,6 @@ class SpotifyApiToken
 
   attr_reader :access_token_type
 
-  def initialize
-    #request_refresh_and_access_tokens code: code
-
-    #    @client_id = ENV['CLIENT_ID']
-     #@token = token
-    #@client_id = ENV['CLIENT_ID']
-    #@code = ENV['SPOTIFY_CODE']
-
-    #@client_token = client_token
-    #@refresh_token = refresh_token
-
-  end
-
   def authorize!
     url = "https://accounts.spotify.com/authorize/"
     response_type = "code"
@@ -33,13 +20,6 @@ class SpotifyApiToken
     @access_token
   end
 
-  # def token
-  #   if @token.nil? || @token_expiration < Time.now
-  #     @token = get_new_token
-  #   end
-  #   @token
-  # end
-
   def get_new_token
     response = HTTParty.post(
         'https://accounts.spotify.com/api/token',
@@ -52,22 +32,6 @@ class SpotifyApiToken
     @access_token_expiration = Time.at(Time.now + response["expires_in"])
     @access_token = response["access_token"]
   end
-
-
-
-
-
-#  def get_new_token
-#    new_token = HTTParty.post(
-#        'https://accounts.spotify.com/api/token',
-#        headers: {"Authorization" => client_id},
-#        body: {
-#          grant_type: "refresh_token",
-#          refresh_token: refresh_token
-#        })
-#    @token_expiration = Time.at(Time.now + new_token["expires_in"])
-#    new_token["token_type"] + " " + new_token["access_token"]
-#  end
 
   def request_refresh_and_access_tokens parameters
     code = parameters["code"]
@@ -97,32 +61,6 @@ class SpotifyApiToken
     ENV['CLIENT_SECRET'] || File.read("./client_secret.txt").chomp
   end
 
-
-  # def SpotifyApiToken.request_refresh_and_access_tokens code:
-  #   url = "https://accounts.spotify.com/api/token"
-  #   response = HTTParty.post(
-  #     url,
-  #     body: {
-  #       grant_type: "authorization_code",
-  #       redirect_uri: 'http://localhost:4567/callback',
-  #       code: code,
-  #       client_id:
-  #       client_secret:
-  #     })
-  #   binding.pry
-  # end
-
-#    new_token = HTTParty.post(
-#        'https://accounts.spotify.com/api/token',
-#        headers: {"Authorization" => client_id},
-#        body: {
-#          grant_type: "refresh_token",
-#          refresh_token: refresh_token
-#        })
-#    @token_expiration = Time.at(Time.now + new_token["expires_in"])
-#    new_token["token_type"] + " " + new_token["access_token"]
-#end
-
   def refresh_token
     unless @refresh_token
       if ENV['REFRESH_TOKEN']
@@ -134,39 +72,15 @@ class SpotifyApiToken
     @refresh_token
   end
 
-  def client_token
-    return @client_token if @client_token
-    if ENV['CLIENT_ID']
-    #if ENV['CLIENT_ID'] && ENV['CLIENT_SECRET']
-      token_string = ENV['CLIENT_ID'] + ":" + ENV['CLIENT_SECRET']
-    else
-      token_file = './token.json'
-      begin
-        raw_token = JSON.parse(File.read token_file)
-      rescue JSON::ParserError
-        raise "There was a problem parsing your token.json file"
-      rescue Errno::ENOENT
-        raise "No 'token.json' file found."
-      end
-
-      if raw_token.values.include? ""
-        raise "'token.json' doesn't include any credentials."
-      end
-      token_string =  raw_token["Client_ID"] + ":" + raw_token["Client_Secret"]
-    end
-    @client_token = "Basic " + Base64.encode64(
-                      token_string
-                    ).sub(/\n/,"")
-  end
 
   def client_creds_encrypted
-    token_string =  client_id + ":" + client_secret
-    "Basic " + Base64.encode64(
-      token_string
-    ).sub(/\n/,"")
-    "Basic " + Base64.strict_encode64(
-      token_string
-    )
+    token_string =
+      client_id +
+      ":" +
+      client_secret
+
+    "Basic " +
+      Base64.strict_encode64(  token_string  )
   end
 
 end
