d309f63fd3ef56747e64950e582dd41bc86b5c48
Maria Cassino <maria.cassino@gmail.com>
2016-06-17 21:14:07 -0400

    commented out method on Time class

--- lib/time.rb
+++ lib/time.rb
@@ -3,9 +3,9 @@ class Time
     self.strftime("%U").to_i
   end
 
-  def convert_to_US_civilian_time(created_at)
-    time = created_at
-    twelve_hour_clock_time = '%m-%d-%Y %I:%M:%S %p'
-    time.strftime(twelve_hour_clock_time)
-  end
+  # def convert_to_US_civilian_time(created_at)
+  #   time = created_at
+  #   twelve_hour_clock_time = '%m-%d-%Y %I:%M:%S %p'
+  #   time.strftime(twelve_hour_clock_time)
+  # end
 end
