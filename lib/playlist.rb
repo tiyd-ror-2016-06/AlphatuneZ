bafc5a1078faaf7f999366c84b2b938d489ccdb8
Zach H <zdhadgraft@gtcc.edu>
2016-06-21 15:05:31 -0400

    Fixed Playlist.* methods

--- lib/playlist.rb
+++ lib/playlist.rb
@@ -12,26 +12,26 @@ class Playlist < ActiveRecord::Base
     Hash[self.alphabetized_hash.map { |k,a| [k,a.max_by {|song| song.total_votes_current_week }]}]
   end
 
-  def Playlist.current
+  def self.current
     Song.all.select { |song| song.suggested_date.week_number == Time.now.week_number }
   end
 
-  def Playlist.current_alpha_hash
-    Hash[(Playlist.current.group_by{ |s| s.alpha_categorize }).sort]
+  def self.current_alpha_hash
+    Hash[(self.current.group_by{ |s| s.alpha_categorize }).sort]
   end
 
-  def Playlist.by_week week=-1
+  def self.by_week week=-1
     pweek = Time.now.week_number + week
-      p = Playlist.create!(created_at: Time.now)
+      p = self.create!(created_at: Time.now)
     Song.all.select { |song| song.suggested_date.week_number == pweek }.each do |s|
       PlaylistSong.create!(song_id: s.id, playlist_id: p.id)
     end
     p
   end
 
-  def Playlist.weekly_winners
-    p = Playlist.by_week.alphabetized_hash
-    wp = Playlist.create!(created_at: Time.now)
+  def self.weekly_winners
+    p = self.by_week.alphabetized_hash
+    wp = self.create!(created_at: Time.now)
     p.each do |letter, songs|
       winner = p[letter].max_by { |song| song.total_votes_current_week}
       PlaylistSong.create!(song_id: winner.id, playlist_id: wp.id)
